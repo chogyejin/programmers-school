f16be102e96fcda3254f05add46e3a7f7ed397bd
chogyejin <whrpwls96@naver.com>
2022-05-13 16:33:28 +0900

    feat: mixin 예제 2 추가

--- TIL/220512/src/App.vue
+++ TIL/220512/src/App.vue
@@ -1,33 +1,61 @@
 <template>
   <div>
-    <!-- count는 mixin, msg는 App의 msg-->
-    <h1>{{ count }}</h1>
-    <h1>{{ msg }}</h1>
-    <Hello />
+    <h1>설문조사</h1>
+    <!-- <TextField v-model="fields[0].value" :title="fields[0].title" />
+    <SimpleRadio
+      v-model="fields[1].value"
+      :title="fields[1].title"
+      :items="fields[1].items"
+    /> -->
+    <component
+      :is="field.component"
+      v-for="field in fields"
+      :key="'component-' + field.title"
+      v-model="field.value"
+      :title="field.title"
+      :items="field.items"
+    />
+    <h1>결과</h1>
+    <div v-for="field in fields" :key="'result-' + field.title">
+      {{ field.value }}
+    </div>
+
+    <button @click="submit">제출</button>
   </div>
 </template>
 
 <script>
-import Hello from "~/components/Hello";
-import sampleMixin from "./mixins/sample";
+// import TextField from "~/components/fields/TextField.vue";
+// import SimpleRadio from "~/components/fields/SimpleRadio.vue";
+import * as FieldComponents from "./components/fields/index";
 
 export default {
-  components: {
-    Hello,
-  },
-  mixins: [sampleMixin],
+  components: { ...FieldComponents },
   data() {
-    return { msg: "안녕ㅇ" };
-  },
-  created() {
-    this.init();
+    return {
+      fields: [
+        {
+          component: "TextField",
+          title: "이름",
+          value: "",
+        },
+        {
+          component: "SimpleRadio",
+          title: "나이대",
+          value: "",
+          items: ["20대", "30대", "40대", "50대"],
+        },
+      ],
+    };
   },
   methods: {
-    async init() {
-      const res = await this.$myFetch(
-        "https://jsonplaceholder.typicode.com/todos/1"
-      );
-      console.log(res);
+    submit() {
+      console.log(this.fields); // proxy 객체
+      const result = this.fields.map(({ title, value }) => ({
+        title,
+        value,
+      }));
+      console.log(result);
     },
   },
 };
---
+++ TIL/220512/src/components/fields/index.js
@@ -0,0 +1,5 @@
+export { default as TextField } from "./TextField";
+export { default as SimpleRadio } from "./SimpleRadio";
+
+// App 컴포넌트에서 * as FieldsComponents로 import 가능
+// components 등록에서 spread
