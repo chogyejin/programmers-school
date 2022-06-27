ee0334e077486ce04cd66b8bee093375921b9d4a
chogyejin <whrpwls96@naver.com>
2022-06-27 16:45:59 +0900

    feat: redux-logger, devtools-extension, persist 적용

--- TIL/220624/src/redux/index.ts
+++ TIL/220624/src/redux/index.ts
@@ -1,9 +1,27 @@
 // root reducer
-import { combineReducers, createStore } from "redux";
 import { tasks } from "./tasks";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { persistReducer, persistStore } from "redux-persist";
+import session from "redux-persist/lib/storage/session";
 
-const rootReducer = combineReducers({ tasks });
+const persistConfig = {
+  key: "root",
+  storage: session, // local storage는 storage
+  whiteList: ["tasks"],
+  // blackList: [],
+};
 
-export const store = createStore(rootReducer);
+const combineReducer = combineReducers({ tasks });
+
+const rootReducer = persistReducer(persistConfig, combineReducer);
+
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof rootReducer>;
